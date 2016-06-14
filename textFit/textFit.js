0f91fdba51468f33b85e5afea9d5f462efb427a0
vosmer <vosmer@orbita1.ru>
2016-06-14 10:38:52 +0300

    fit text

---
+++ textFit/textFit.js
@@ -0,0 +1,18 @@
+// утф8
+//<!--
+//"use strict";
+console.log('fitText');
+if(typeof(vosmerLib)=="undefined"){
+	vosmerLib={"gv":{},"gm":{}};
+	
+}
+vosmerLib.gm.textFitArea=function(h,w){
+	//TODO check if element or sizes given as parameters
+	var th=this;
+	//var fntSize=(Math.pow(((bannerW*bannerH)/75),(1/2)))*(4/5)+'px';
+	
+};
+window.onload=function(){
+
+};
+//-->
